84f49e52a5b7bc6ba5394f6347f823f67e274699
wangding <408542507@qq.com>
2019-03-18 19:18:19 +0800

    master

---
+++ rectangle.js
@@ -0,0 +1,19 @@
+
+function(){
+
+    //console.log('hello');/
+    var $width=$('#width'),
+        $height=$('#height'),
+        $btnCal=$('#btn'),
+        $perimeter=$('#perimeter'),
+        $area=$('#area');
+        $btnCal.click(function(){
+     var w=Number($width.val()),
+            h=Number($height.val());
+    var r=new Rectangle(w,h);
+                                        
+     $perimeter.val(r.perimeter());
+        $area.val(r.area)) ;
+
+       })
+})
